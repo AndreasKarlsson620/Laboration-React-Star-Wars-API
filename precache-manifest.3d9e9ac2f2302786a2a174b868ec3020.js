c2934d6afd5d2fd2e05a23bab0c8e06010c7e086
Andreas Karlsson <andreas.karlsson90@hotmail.com>
2020-06-18 18:49:48 +0200

    Updates

---
+++ precache-manifest.3d9e9ac2f2302786a2a174b868ec3020.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc49df3667fda92277bcb23da84f2a5d",
+    "url": "/Laboration-React-Star-Wars-API/index.html"
+  },
+  {
+    "revision": "92598e823db4bab6dab1",
+    "url": "/Laboration-React-Star-Wars-API/static/css/main.92e5760c.chunk.css"
+  },
+  {
+    "revision": "d326f0f034b96e6851fa",
+    "url": "/Laboration-React-Star-Wars-API/static/js/2.7eda31b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Laboration-React-Star-Wars-API/static/js/2.7eda31b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92598e823db4bab6dab1",
+    "url": "/Laboration-React-Star-Wars-API/static/js/main.7699edb9.chunk.js"
+  },
+  {
+    "revision": "199f91cfc5f4a293c5cd",
+    "url": "/Laboration-React-Star-Wars-API/static/js/runtime-main.7f788b38.js"
+  }
+]);
